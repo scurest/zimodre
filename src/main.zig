30ecf670c54603b0b804824a85cb66f2032336a2
scurest <scurest@users.noreply.github.com>
2017-10-22 23:12:01 -0500

    Better CLI arg handling

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const mem = @import("std").mem;
 const Buffer = @import("std").Buffer;
 
 const mod = @import("mod.zig");
@@ -22,27 +23,68 @@ pub fn main() -> %void {
 
 fn main2() -> %void {
     // Parse CLI arguments
-    const path = {
-        var args_it = std.os.args();
-        const exe = %return ??args_it.next(&std.mem.c_allocator);
-        var path: ?[]const u8 = null;
-        while (args_it.next(&std.mem.c_allocator)) |arg_or_err| {
-            const arg = %return arg_or_err;
-            if (path != null) {
-                return error.CLIBadArguments;
-            }
-            path = arg;
+
+    var maybe_in_path: ?Buffer = null;
+    defer if (maybe_in_path) |*b| { b.deinit(); };
+    var maybe_out_path: ?Buffer = null;
+    defer if (maybe_out_path) |*b| { b.deinit(); };
+    var verbose = false;
+    var help = false;
+    var dry_run = false;
+
+    var args = std.os.args();
+    _ = args.skip(); // skip exe name
+    while (args.next(&mem.c_allocator)) |arg_or_err| {
+        const arg = %return arg_or_err;
+        defer mem.c_allocator.free(arg);
+
+        if (mem.eql(u8, "-h", arg) or mem.eql(u8, "--help", arg)) {
+            help = true;
+            break;
         }
-        path ?? return error.CLINoInputFile
-    };
+        if (mem.eql(u8, "-v", arg) or mem.eql(u8, "--verbose", arg)) {
+            verbose = true;
+            continue;
+        }
+        if (mem.eql(u8, "--dry-run", arg)) {
+            dry_run = true;
+            continue;
+        }
+        if (arg[0] == '-') {
+            return error.CLIBadArguments;
+        }
+
+        if (maybe_in_path == null) {
+            maybe_in_path = %%Buffer.init(&mem.c_allocator, arg);
+        } else if (maybe_out_path == null) {
+            maybe_out_path = %%Buffer.init(&mem.c_allocator, arg);
+        } else {
+            // Too many arguments
+            return error.CLIBadArguments;
+        }
+    }
+
+    if (help) {
+        %%std.io.stderr.printf("zimodre: convert Monster Hunter Stories MOD files to GLB\n");
+        usage();
+        return;
+    }
+
+    var in_path = maybe_in_path ?? return error.CLIBadArguments;
+    maybe_in_path = null;
+    defer in_path.deinit();
+    var out_path = maybe_out_path ?? return error.CLIBadArguments;
+    maybe_out_path = null;
+    defer out_path.deinit();
+
 
     // Do the conversion
 
-    var input_file = %return read_file(path);
+    var input_file = %return read_file(in_path.toSliceConst());
     defer input_file.deinit();
 
     var parser = Parser.new(input_file.toSliceConst());
-    parser.logging_on = true;
+    parser.logging_on = verbose;
     var mod_file = %return mod.parse(&parser);
     defer mod_file.deinit();
 
@@ -52,15 +94,17 @@ fn main2() -> %void {
     var glb_buffer = %return gltf.write_glb(&g);
     defer glb_buffer.deinit();
 
-    %return write_file("out.glb", glb_buffer.toSlice());
+    if (!dry_run) {
+        %return write_file(out_path.toSliceConst(), glb_buffer.toSlice());
+    }
 }
 
 /// Read a whole file into a Buffer.
 fn read_file(path: []const u8) -> %Buffer {
-    var buffer = Buffer.initNull(&std.mem.c_allocator);
+    var buffer = Buffer.initNull(&mem.c_allocator);
     %defer buffer.deinit();
 
-    var in = std.io.InStream.open(path, &std.mem.c_allocator)
+    var in = std.io.InStream.open(path, &mem.c_allocator)
         %% return error.CouldntOpenInputFile;
     defer in.close();
     in.readAll(&buffer) %% return error.CouldntReadInputFile;
@@ -69,7 +113,7 @@ fn read_file(path: []const u8) -> %Buffer {
 }
 
 fn write_file(path: []const u8, contents: []const u8) -> %void {
-    var out = std.io.OutStream.open(path, &std.mem.c_allocator)
+    var out = std.io.OutStream.open(path, &mem.c_allocator)
         %% return error.CouldntOpenOutputFile;
     defer out.close();
     out.write(contents) %% return error.CouldntWriteOutputFile;
@@ -112,5 +156,12 @@ fn print_error(err: error) {
 
 fn usage() {
     var o = std.io.stderr;
-    %%o.printf("Usage: zimodre [input]\n");
+    %%o.printf(
+        \\Usage: zimodre [options...] <input> <output>
+        \\Options:
+        \\  --dry-run        Don't write the output file
+        \\  -v, --verbose    Show debugging output
+        \\  -h, --help       Show this help message
+        ++"\n"
+    );
 }
