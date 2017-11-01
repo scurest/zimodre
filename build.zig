52dbb80e244ca1d29b992e7c02a3ed6eedb74cc5
scurest <scurest@users.noreply.github.com>
2017-10-31 21:19:21 -0500

    build: use standard release options

--- build.zig
+++ build.zig
@@ -3,10 +3,8 @@ const builtin = @import("builtin");
 
 pub fn build(b: &Builder) {
     var exe = b.addExecutable("zimodre", "src/main.zig");
-
+    exe.setBuildMode(b.standardReleaseOptions());
     exe.linkSystemLibrary("c");
-
     b.default_step.dependOn(&exe.step);
-
     b.installArtifact(exe);
 }
